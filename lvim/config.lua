a2c5ccd319f362f573c00d97db84785eb0a91f1a
Gustavo Antonio Martini <gutamen@live.com>
2023-09-29 10:14:02 -0300

    organização em pasta

---
+++ lvim/config.lua
@@ -0,0 +1,14 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+
+
+vim.opt.cmdheight = 1                           -- Mais espaço na linha de comando do NVim para exibir mensagens
+vim.opt.shiftwidth = 4                          -- Número de espaços inseridos por identação
+vim.opt.tabstop = 4                             -- Insere 2 espaços para cada TAB
+vim.opt.relativenumber = false                  -- Número relativo de linhas
+vim.opt.wrap = true                             -- Quebra de linha, quando não tem espaço o suficiente na tela
+lvim.colorscheme = "industry"                   -- Tema selecionado
+lvim.builtin.lualine.style = "default"          -- Configuração da barra de status do editor de texto
+
